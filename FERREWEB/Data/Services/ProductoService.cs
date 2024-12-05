30e9e697ac0dc2d8ea26cc8a83d7ae92f0cbdb27
Alex5727 <alejandrogrjalva@gmail.com>
2024-12-05 14:59:09 -0700

    #

---
+++ FERREWEB/Data/Services/ProductoService.cs
@@ -0,0 +1,39 @@
+﻿using FERREWEB.Data.Models;
+using FERREWEB.Data.ViewModels;
+using FERREWEB.Exceptions;
+using System.Text.RegularExpressions;
+
+namespace FERREWEB.Data.Services
+{
+    public class ProductoService
+    {
+        private AppDbContext _context;
+
+        public ProductoService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //metodo crear Marca
+        public Producto AddMarca(ProductoVM marca)
+        {
+            if (StringStartsWithNumber(marca.NombreProducto))
+                throw new NameException("El nombre empieza con un número",
+                marca.NombreProducto);
+
+            var _marca = new Producto()
+            {
+                NombreProducto = marca.NombreProducto,
+                Costo = marca.Costo,
+                Imagen = marca.Imagen,
+                Stock = marca.Stock
+            };
+            _context.Productos.Add(_marca);
+            _context.SaveChanges();
+
+            return _marca;
+        }
+
+        private bool StringStartsWithNumber(string name) => (Regex.IsMatch(name, @"^\d"));
+    }
+}
---
+++ FERREWEB/Data/ViewModels/ProductoVM.cs
@@ -0,0 +1,30 @@
+﻿namespace FERREWEB.Data.ViewModels
+{
+    public class ProductoVM
+    {
+        public string NombreProducto { get; set; }
+        public int Costo { get; set; }
+        public string Imagen { get; set; }
+        public int Stock { get; set; }
+    }
+
+    public class ProductoDataVM
+    {
+        public string NombreProducto { get; set; }
+        public int Costo { get; set; }
+        public string Imagen { get; set; }
+        public int Stock { get; set; }
+        public List<ProductowithCategoriaVM> Categoria { get; set; }
+        public List<ProductowithMarcaVM> Marca { get; set; }
+    }
+
+    public class ProductowithMarcaVM
+    {
+        public string NombreMarca { get; set; }
+    }
+
+    public class ProductowithCategoriaVM
+    {
+        public string NombreCategoria { get; set; }
+    }
+}
